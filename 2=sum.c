d5099f47b9f1f3d6e483d6cb16921b7266693853
Karthik.S.Kashyap <119308887+Karthik1772@users.noreply.github.com>
2023-02-08 09:13:49 -0500

    Add files via upload

---
+++ 2=sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("enter the 2 no :");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum = %d",sum);
+}
---
+++ 3=arsquare.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int r;
+float area;
+printf("enter the radius:");
+scanf("%d",&r);
+area=r*r;
+printf("area = %f",area);
+}
